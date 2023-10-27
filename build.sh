24fba6fe445f3250444546ad19a970d3f6bdba7f
Domiciano Rincón <domiciano.rincon@correounivalle.edu.co>
2023-10-27 14:25:28 -0500

    Semana 14 build

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 
 echo "Collection statics..."
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput --clear
